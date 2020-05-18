5e9e68eefecd96f3294226f066d8770cdb89d18f
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-05-19 02:38:53 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,90 @@
-
+let imageShow = document.getElementsByClassName('nested-image');
+let positionHolder = 0;
+// for (image of imageShow) {
+// 	image.style.zIndex = 1;
+// }
+let backBtn = document.getElementsByTagName('button')[0];
+console.log(backBtn);
+backBtn.addEventListener('click', function() {
+	for (image of imageShow) {
+		image.style.zIndex = -1;
+	}
+	if (positionHolder === 0) {
+		positionHolder = imageShow.length - positionHolder - 1;
+		return (imageShow[positionHolder].style.zIndex = 1);
+	} else {
+		positionHolder = positionHolder - 1;
+		return (imageShow[positionHolder].style.zIndex = 1);
+	}
+});
+let forwardBtn = document.getElementsByTagName('button')[1];
+console.log(forwardBtn);
+forwardBtn.addEventListener('click', function() {
+	for (image of imageShow) {
+		image.style.zIndex = -1;
+	}
+	if (positionHolder === imageShow.length - 1) {
+		positionHolder = 0;
+		return (imageShow[positionHolder].style.zIndex = 1);
+	} else {
+		positionHolder = positionHolder + 1;
+		return (imageShow[positionHolder].style.zIndex = 1);
+	}
+});
+let randomBtn = document.getElementsByTagName('button')[2];
+// let stop = setInterval(function() {
+// 	for (image of imageShow) {
+// 		image.style.zIndex = -1;
+// 	}
+// 	let randomiser = Math.floor(Math.random() * imageShow.length);
+// 	return (imageShow[randomiser].style.zIndex = 1);
+// }, 5000);
+// randomBtn.addEventListener(
+// 	'click',
+// 	setInterval(function() {
+// 		for (image of imageShow) {
+// 			image.style.zIndex = -1;
+// 		}
+// 		let randomiser = Math.floor(Math.random() * imageShow.length);
+// 		return (imageShow[randomiser].style.zIndex = 1);
+// 	}, 1000)
+// );
+// console.log(stop);
+// let stopBtn = document.getElementsByTagName('button')[3];
+// stopBtn.addEventListener('click', function() {
+// 	return clearInterval(stop);
+// });
+// =====================
+// let backBtn = document.getElementsByTagName('button')[0];
+// console.log(backBtn);
+// backBtn.addEventListener('click', function() {
+// 	for (image of imageShow) {
+// 		image.style.zIndex = -1;
+// 	}
+// 	let randomiser = Math.floor(Math.random() * imageShow.length);
+// 	return (imageShow[randomiser].style.zIndex = 1);
+// });
+// let forwardBtn = document.getElementsByTagName('button')[1];
+// console.log(forwardBtn);
+// forwardBtn.addEventListener('click', function() {
+// 	for (image of imageShow) {
+// 		image.style.zIndex = -1;
+// 	}
+// 	let randomiser = Math.floor(Math.random() * imageShow.length);
+// 	return (imageShow[randomiser].style.zIndex = 1);
+// // });
+// let stop = setInterval(function() {
+// 	for (image of imageShow) {
+// 		image.style.zIndex = -1;
+// 	}
+// 	let randomiser = Math.floor(Math.random() * imageShow.length);
+// 	return (imageShow[randomiser].style.zIndex = 1);
+// }, 5000);
+// setInterval(function() {
+// 	for (image of imageShow) {
+// 		image.style.zIndex = -1;
+// 	}
+// 	let randomiser = Math.floor(Math.random() * imageShow.length);
+// 	return (imageShow[randomiser].style.zIndex = 1);
+// }, 5000);
+// clearInterval(stop);
