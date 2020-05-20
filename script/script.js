496c98d22dd67436202ab50197f058041a460f22
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-05-20 10:33:24 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -9,13 +9,23 @@ backBtn.addEventListener('click', backWard);
 let forwardBtn = document.getElementsByTagName('button')[1];
 
 forwardBtn.addEventListener('click', forWard);
+let randomBtn = document.getElementById('random');
+let stopSlide;
+let stopBtn = document.getElementById('stop');
+
+stopBtn.addEventListener('click', () => clearInterval(stopSlide));
+randomBtn.addEventListener('click', randSlide);
 
 addEventListener('keydown', function(event) {
-	if (event.which == 37) {
+	if (event.which === 37) {
 		backWard();
 	} else if (event.which === 39) {
 		forWard();
 	}
+	let stopSlide;
+	// if (event.which === 38) {
+	// 	stopShow = setInterval(randomShow, 3000);
+	// }
 });
 
 function backWard() {
@@ -55,16 +65,18 @@ function forWard() {
 		iconBorder[positionHolder].style.border = '3px solid yellow';
 	}
 }
-
-// let randomBtn = document.getElementsByTagName('button')[2];
-
-// function randomChange() {
-// 	for (image of imageShow) {
-// 		image.style.zIndex = -1;
-// 	}
-// 	let randomiser = Math.floor(Math.random() * imageShow.length);
-// 	imageShow[randomiser].style.zIndex = 1;
-// }
-// randomBtn.addEventListener('click', function() {
-// 	setInterval((randomChange, 5000));
-// });
+function randomShow() {
+	for (image of imageShow) {
+		image.style.zIndex = -1;
+	}
+	for (border of iconBorder) {
+		border.style.border = '1px solid grey';
+	}
+	let randomiser = Math.floor(Math.random() * imageShow.length);
+	imageShow[randomiser].style.zIndex = 1;
+	iconBorder[randomiser].style.border = '3px solid purple';
+}
+function randSlide() {
+	stopSlide = setInterval(randomShow, 3000);
+}
+// setTimeout(noAction, 10000);
